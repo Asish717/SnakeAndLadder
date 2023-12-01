5e1d8d10e580a6fcb636b1a862ff40508250b33d
Asish <asishkumar717@gmail.com>
2023-12-01 15:13:49 +0530

    Added snake ladder dice roll to get numbers between 1to6

--- src/main/java/com/java/SnakeLadder.java
+++ src/main/java/com/java/SnakeLadder.java
@@ -1,9 +1,19 @@
 package com.java;
 
+import java.util.Random;
+
 public class SnakeLadder {
     public static final int BoardSize=100;
 
     public static void main(String[] args) {
         int playerPosition=0;
+        Random random=new Random();
+        int ran=random.nextInt(7);
+        if (ran==0){
+            int dice=ran+1;
+            System.out.println(dice);
+        } else {
+            System.out.println(ran);
+        }
     }
 }
