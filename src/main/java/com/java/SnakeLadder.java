77333060351e92b1cc197e83b420b48129b10e33
Asish <asishkumar717@gmail.com>
2023-12-04 16:39:50 +0530

    Added dice roll count and position after each roll

--- src/main/java/com/java/SnakeLadder.java
+++ src/main/java/com/java/SnakeLadder.java
@@ -7,19 +7,22 @@ public class SnakeLadder {
     public static Random random = new Random();
     public static void main(String[] args) {
         int playerPosition = 0;
+        int diceRollCount=0;
         while (playerPosition < BoardSize) {
+            diceRollCount++;
+            int diceRoll=(int)(Math.random()*6)+1;
             int option = random.nextInt(3);
             switch (option) {
                 case 0:
                     System.out.println("No play: player stays in the same position");
                     break;
                 case 1:
-                    System.out.println("Ladder: player moves ahead by dice position");
-                    playerPosition += (int)(Math.random()*6)+1;
+                    System.out.println("Ladder: player moves ahead by "+diceRoll+" position");
+                    playerPosition += diceRoll;
                     break;
                 case 2:
-                    System.out.println("Snake: player moves behind by dice position");
-                    playerPosition -= (int)(Math.random()*6)+1;
+                    System.out.println("Snake: player moves behind by "+diceRoll+" position");
+                    playerPosition -= diceRoll;
                     break;
             }
             if (playerPosition<0){
@@ -30,5 +33,6 @@ public class SnakeLadder {
             }
             System.out.println(playerPosition);
         }
+        System.out.println("Congratulation on reaching 100 with "+diceRollCount+" dice rolls");
     }
 }
